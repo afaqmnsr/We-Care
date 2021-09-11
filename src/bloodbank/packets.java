f2231a9ab8a16580fe0b60fc211722f798fbb7c1
Afaq Mansoor Khan <afaqmnsr@yahoo.com>
2021-09-11 18:00:03 +0500

    Add files via upload

---
+++ src/bloodbank/packets.java
@@ -0,0 +1,127 @@
+
+package bloodbank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
+import net.proteanit.sql.DbUtils;
+
+
+public class packets extends javax.swing.JFrame {
+
+   
+    public packets() {
+        initComponents();
+         setLocationRelativeTo(null);
+        this.getDataa();
+    }
+
+    void getDataa() //for getting latest data from table
+{
+try
+{
+
+ Class.forName("com.mysql.jdbc.Driver");
+Connection cn=DriverManager.getConnection("jdbc:mysql:///blood","root","");
+
+DefaultTableModel d1=(DefaultTableModel)table.getModel();
+PreparedStatement smt=cn.prepareStatement("select group11,packets from quantityy");
+ResultSet rs=smt.executeQuery();
+
+while(rs.next())
+{
+    
+String a1=rs.getString(1);
+String a2=rs.getString(2);
+
+Object pc[]={a1,a2};
+d1.addRow(pc);
+
+}      
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+
+}
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Blood Group", "Packets"
+            }
+        ));
+        jScrollPane1.setViewportView(table);
+
+        jButton1.setText("Cancel");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(61, 61, 61)
+                        .addComponent(jButton1)))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+         first obj=new first();
+        obj.setVisible(true);
+        this.dispose();        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+   
+    public static void main(String args[]) {
+     
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new packets().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable table;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/bloodbank/t5.java
@@ -0,0 +1,16 @@
+
+package bloodbank;
+import java.text.*;  
+import java.util.*;
+
+
+class t5 {
+
+//    static Object getDate() {
+//        Date date = Calendar.getInstance().getTime();  
+//            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  
+//            String strDate = dateFormat.format(date);  
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    
+}
